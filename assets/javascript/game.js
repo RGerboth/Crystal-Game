32b7819ccf022db7b2c147d3cc6ae1a1750cb656
Robert Gerboth <robert.gerboth@gmail.com>
2018-01-24 11:00:21 -0700

    Styling updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,19 +25,14 @@
       // For each iteration, we will create an imageCrystal
 
       var imageCrystal = $("<img>");
-      // First each crystal will be given the class ".crystal-image".
-      // This will allow the CSS to take effect.
       imageCrystal.addClass("crystal-image");
 
-      // Each imageCrystal will be given a src link to the crystal image
-      imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+      // imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+      imageCrystal.attr("src", "assets/images/quartzCrystal.jpg");
 
-      // Each imageCrystal will be given a data attribute called data-crystalValue.
-      // This data attribute will be set equal to the array value.
       var crystalValue = (Math.floor(Math.random() * 12 ) + 1);
       imageCrystal.attr("data-crystalvalue", crystalValue);
 
-      // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
       $("#crystals").append(imageCrystal);
       console.log("Crystal " + i + " value: " + crystalValue)
     }
